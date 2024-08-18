4b06ab332579022bf5df957165b642013c5a612e
yaraaos <yara144ss@gmail.com>
2024-08-19 00:44:50 +0300

    Task  1,2,3

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+/* Задача 1. Генератор slug */
+
+function slugify(title) {
+    const lowerTitle = title.toLowerCase();
+    const slugArr = lowerTitle.split(" ");
+    const slug = slugArr.join("-");
+
+return slug;   
+}
+
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
---
+++ js/task-3.js
@@ -0,0 +1,18 @@
+/* Задача 3. Фільтрація масиву чисел */
+
+function filterArray(numbers, value) {
+    const bigger = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > value) {
+            bigger.push(numbers[i]);
+        }
+    }
+    return bigger;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
